dda801c0dab10fa182b2ccf3c7823b9070fa3576
hotteokButler <ygs12567@gmail.com>
2025-06-13 16:26:43 +0900

    Add: main page input validation check - zod 라이브러리 사용

--- src/components/input-field.tsx
+++ src/components/input-field.tsx
@@ -7,30 +7,57 @@ import React, { useState } from 'react';
 
 import style from '@/asset/css/custom.module.css';
 import { inputToolTip } from '@/store/input-toolTip-text';
+import { validateSchema, ValidateSchemaType } from '@/util/validate';
 
 import CustomizeRequiredMark from './required-mark';
 import { TelPreFix } from './tel-prefix-selector';
 
 export default function InputField() {
-	const [form] = Form.useForm();
+	const [form] = Form.useForm<ValidateSchemaType>();
 	const [componentDisabled, setComponentDisabled] = useState<boolean>(true);
 
+	const validateField = <K extends keyof typeof validateSchema.shape>(field: K) => {
+		const fieldSchema = validateSchema.shape[field] as z.ZodTypeAny;
+
+		return async (_: unknown, value: unknown) => {
+			const result = fieldSchema.safeParse(value);
+			if (!result.success) {
+				throw new Error(result.error.errors[0].message);
+			}
+		};
+	};
 	return (
 		<Form layout='vertical' form={form} requiredMark={CustomizeRequiredMark}>
-			<Form.Item name='title' label='Title' rules={[{ required: true, message: '홈페이지 타이틀을 입력해 주세요' }]} tooltip={inputToolTip.title}>
+			<Form.Item name='title' label='Title' rules={[{ required: true, message: '홈페이지 타이틀을 입력해 주세요' }, { validator: validateField('title') }]} tooltip={inputToolTip.title}>
 				<Input placeholder='홈페이지 타이틀' />
 			</Form.Item>
-			<Form.Item name='description' label='Description' rules={[{ required: true, message: '홈페이지 설명을 입력해 주세요' }]} tooltip={inputToolTip.description}>
+			<Form.Item
+				name='description'
+				label='Description'
+				rules={[{ required: true, message: '홈페이지 설명을 입력해 주세요' }, { validator: validateField('description') }]}
+				tooltip={inputToolTip.description}
+			>
 				<TextArea rows={3} placeholder='홈페이지 설명' />
 			</Form.Item>
 			<Form.Item name='keyword' label='Keyword' tooltip={inputToolTip.keyword}>
 				<Input placeholder='콤마(,) 로 구분해서 작성해주세요 예: 키워드1, 키워드2,' />
 			</Form.Item>
 			<Alert message='Google에서는 더 이상 keyword meta tag를 수집하지 않습니다.' style={{ marginBottom: '1.8em' }} />
-			<Form.Item name='url' label='URL' required={true} rules={[{ required: true, message: '홈페이지 대표 링크를 프로토콜(http, https)과 함께 입력해 주세요' }]} tooltip={inputToolTip.url}>
+			<Form.Item
+				name='url'
+				label='URL'
+				required={true}
+				rules={[{ required: true, message: '홈페이지 대표 링크를 프로토콜(http, https)과 함께 입력해 주세요' }, { validator: validateField('url') }]}
+				tooltip={inputToolTip.url}
+			>
 				<Input placeholder='홈페이지 대표링크' />
 			</Form.Item>
-			<Form.Item name='thumb' label='Thumbnail' required={true} tooltip={inputToolTip.thumbnail}>
+			<Form.Item
+				name='thumb'
+				label='Thumbnail'
+				rules={[{ required: false, message: '홈페이지 대표 썸네일 이미지 링크를 입력해 주세요' }, { validator: validateField('thumb') }]}
+				tooltip={inputToolTip.thumbnail}
+			>
 				<Input placeholder='홈페이지 썸네일 이미지' />
 			</Form.Item>
 			<div>
@@ -47,7 +74,7 @@ export default function InputField() {
 					<Form.Item name='logo' label='Logo' tooltip={inputToolTip.logo}>
 						<Input placeholder='로고 링크' disabled={componentDisabled} />
 					</Form.Item>
-					<Form.Item name='tel' label='전화번호' rules={[{ required: false, message: '전화번호를 입력해 주세요' }]} tooltip={inputToolTip.url}>
+					<Form.Item name='tel' label='전화번호' tooltip={inputToolTip.url} rules={[{ required: false }, { validator: validateField('tel') }]}>
 						<Input addonBefore={<TelPreFix />} style={{ width: '100%' }} disabled={componentDisabled} />
 					</Form.Item>
 					<Typography.Text className={style.input_label}>{CustomizeRequiredMark('SNS 링크', { required: false })}</Typography.Text>
@@ -56,7 +83,13 @@ export default function InputField() {
 							<>
 								{fields.map(({ key, name, ...restField }) => (
 									<Space key={key} align='baseline' style={{ width: '100%' }}>
-										<Form.Item {...restField} name={[name, `sns_link_${key + 1}`]} style={{ width: '100%' }} tooltip={inputToolTip.url}>
+										<Form.Item
+											{...restField}
+											name={[name, `sns_link_${key + 1}`]}
+											style={{ width: '100%' }}
+											rules={[{ required: false }, { validator: validateField('sns') }]}
+											tooltip={inputToolTip.url}
+										>
 											<Input placeholder='SNS 링크' style={{ width: '100%' }} disabled={componentDisabled} />
 										</Form.Item>
 										<MinusCircleOutlined
---
+++ src/util/validate.ts
@@ -0,0 +1,44 @@
+import { z } from 'zod';
+
+export const validateSchema = z.object({
+	title: z
+		.string()
+		.min(10, {
+			message: '최소 10자 이상 작성해주세요.',
+		})
+		.max(60, {
+			message: '60자 이내로 작성해주세요.',
+		}) //길이 제한
+		.regex(/^[\w\s\p{Script=Hangul}\p{Script=Han}\p{Script=Hiragana}\p{Script=Katakana}\|\-:,'"[\]]+$/u, {
+			message: '허용된 문자(특수문자는 ,"\'|-:[]만)만 입력할 수 있습니다.',
+		}),
+	description: z
+		.string()
+		.min(10, {
+			message: '최소 10자 이상 작성해주세요.',
+		})
+		.max(150, {
+			message: '최대 150자 이내로 작성해주세요.',
+		}) //길이 제한
+		.regex(/^[\w\s\p{Script=Hangul}\p{Script=Han}\p{Script=Hiragana}\p{Script=Katakana}\|\-:,'"[\]]+$/u, {
+			message: '허용된 문자(특수문자는 ,"\'|-:[]만)만 입력할 수 있습니다.',
+		}),
+	keyword: z
+		.string()
+		.regex(/^[A-Za-z0-9가-힣, ]*$/, { message: '콤마로 구분된 키워드만 입력 가능합니다' })
+		.optional()
+		.or(z.literal('')),
+	url: z.string().url({ message: '정확한 URL이어야 합니다.' }),
+	thumb: z
+		.string()
+		.url({ message: '정확한 이미지 URL이어야 합니다.' })
+		.regex(/\.(jpe?g|png|gif)$/i, { message: '이미지 URL만 허용됩니다.' }),
+	logo: z
+		.string()
+		.url({ message: '정확한 로고 이미지 URL이어야 합니다.' })
+		.regex(/\.(jpe?g|png|gif)$/i, { message: '이미지 URL만 허용됩니다.' }),
+	tel: z.string().regex(/^[0-9]{2,4}(?:-[0-9]{3,4}){1,2}$/, "전화번호는 '000-0000-0000' 형태여야 합니다"),
+	sns: z.string().url({ message: '정확한 URL이어야 합니다.' }),
+});
+
+export type ValidateSchemaType = z.infer<typeof validateSchema>;
